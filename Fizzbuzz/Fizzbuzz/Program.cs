d950a3b87e52da4dea2e00302e1102b3212dc692
RobinJobb <114588706+RobinJobb@users.noreply.github.com>
2024-03-15 12:59:17 +0100

    Add files via upload

---
+++ Fizzbuzz/Fizzbuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Program
+{
+    public static void Fizzbuzz(int Number)
+    {
+        if (Number % 3 == 0)
+        {
+            Console.WriteLine("Fizz");
+        }
+        else if (Number % 5 == 0)
+        {
+            Console.WriteLine("Buzz");
+        }
+        else if (Number % 3 == 0 && Number % 5 == 0)
+        {
+            Console.WriteLine("FizzBuzz");
+        }
+        else
+        {
+            Console.WriteLine(Number);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        // Call the Fizzbuzz function with an example number
+        Fizzbuzz(15);
+    }
+}
